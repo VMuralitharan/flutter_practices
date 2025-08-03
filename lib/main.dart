cd46c743a0b367495d3d9944ce8a1869381f236e
VMuralitharan <vijayakumarmuralitharan@gmail.com>
2025-08-03 23:02:26 +0530

    building custom widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,34 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.purple, const Color.fromARGB(255, 91, 53, 197)],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: Center(child: Text("hello hi")),
-        ),
+        appBar: AppBar(title: Text("New Project")),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.purple, const Color.fromARGB(255, 91, 53, 197)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(
+        child: Text(
+          "hello hi",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontFamily: 'Roboto',
+          ),
+        ),
+      ),
+    );
+  }
 }
